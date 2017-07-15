4955b0374fff27ee26c4b5782f1f1c90c78eb54a
WillJHaggard <willjhaggard@gmail.com>
2017-07-15 17:44:50 -0400

    Edit structure/allow .js files via .eslintrc.json

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
+
+render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,31 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './js/App.js',
-  devtool: 'source-map',
+  entry: path.join(__dirname, 'src/App.js'),
+  devtool: 'cheap-eval-source-map',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  devServer: {
+    publicPath: '/public/',
+  },
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
+    extensions: ['.js', '.jsx'],
   },
   stats: {
     colors: true,
     reasons: true,
-    chunks: false,
+    chunks: true,
   },
   module: {
     rules: [
+      {
+        enforce: 'pre',
+        test: /\.js?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+      },
       {
         test: /\.js?$/,
         loader: 'babel-loader',
