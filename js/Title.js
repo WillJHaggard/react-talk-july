3af8d992994a983536a54c7ac8e479d682f16ecc
WillJHaggard <willjhaggard@gmail.com>
2017-07-15 17:07:29 -0400

    simple jsx refactor of Title and clientApp

--- js/Title.js
+++ js/Title.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-function MyTitle() {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement('h1', null, 'Check out this component!')
+const MyTitle = props => {
+  const style = {color: props.color};
+  return (
+    <div>
+      <h1 style={style}>
+        {props.title}
+      </h1>
+    </div>
   );
-}
+};
 
 export default MyTitle;
