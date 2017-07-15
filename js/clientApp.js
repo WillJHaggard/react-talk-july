3fc36b6d9c9507a32f4f4c1cc72e89b987a1e6f1
WillJHaggard <willjhaggard@gmail.com>
2017-07-15 17:07:29 -0400

    simple jsx refactor of Title and clientApp

--- js/clientApp.js
+++ js/clientApp.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {render} from 'react-dom';
+import MyTitle from './Title';
 
-function MyFirstComponent() {
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(MyTitle, null),
-    React.createElement(MyTitle, null),
-    React.createElement(MyTitle, null)
+const MyFirstComponent = () => {
+  return (
+    <div>
+      <MyTitle title="Props are great!" color="rebeccapurple" />
+      <MyTitle title="Use props everywhere!" color="mediumaquamarine" />
+      <MyTitle title="Props are the best!" color="peru" />
+    </div>
   );
-}
+};
 
 render(React.createElement(MyFirstComponent), document.getElementById('app'));
